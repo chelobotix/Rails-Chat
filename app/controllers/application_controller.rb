5634034e4019764c1b8951a08c26deb7b96b6bb0
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-12-26 10:24:02 -0400

    add skip_before_action :verify_authenticity_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+
   helper_method :current_user, :logged_in?
   # current_user
   def current_user
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-  skip_before_action :verify_authenticity_token
   before_action :require_user
   # post -> Create
   def create
